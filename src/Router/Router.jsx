97ae4cbf6b7db724b0936cf3150831abdb49e21a
Topan-Roy <roytopan734@gmail.com>
2025-11-12 23:10:20 +0600

    added Book-shope-app-client project setup

---
+++ src/Router/Router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "../Layout/RootLayout";
+import Home from "../Components/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:RootLayout,
+    children:[
+        {
+            index:true,
+            Component:Home
+        }
+    ]
+  },
+]);
